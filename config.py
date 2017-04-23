c88201e10a48e186fe5513178b83ebd8a795eaf5
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-04-24 02:15:20 +0530

    Config file added

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
 # config.py
-DEBUG = True
 
+class Config(object):
+    """
+     Common configurations
+    """
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+app_config = {
+        'development': DevelopmentConfig,
+        'production' : ProductionConfig
+    }
