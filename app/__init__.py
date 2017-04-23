851c0d44a8a259166edbd795e622a2e400c8b3eb
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-04-24 04:04:04 +0530

    Config file modified

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,21 @@
 # app/__init__.py
 
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from config import app_config
 
-app = Flask(__name__, instance_relative_config=True)
+db = SQLAlchemy()
 
-from app import views
+def create_app(config_name):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
 
-app.config.from_object('config')
+#temp route
+    @app.route('/')
+    def start():
+        return 'yayyyy'
+
+    return app
+
--- run.py
+++ run.py
@@ -1,6 +1,10 @@
 # run.py
 
-from app import app
+import os
+from app import create_app
+
+config_name = os.getenv('FLASK_CONFIG')
+app = create_app(config_name)
 
 if __name__ == "__main__":
     app.run()
