7e791b82daf6acca5b39f1d4c08bc129017e9eee
Abhishek Gupta <abhishekmodi2011@gmail.com>
2017-04-30 22:14:57 +0530

    Config changed for unittests

--- config.py
+++ config.py
@@ -4,15 +4,28 @@ class Config(object):
     """
      Common configurations
     """
+    DEBUG = True
 
 class DevelopmentConfig(Config):
-    DEBUG = True
+    """
+    Development configurations
+    """
     SQLALCHEMY_ECHO = True
 
 class ProductionConfig(Config):
+    """
+    Production configurations
+    """
     DEBUG = False
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+    TESTING = True
 
 app_config = {
         'development': DevelopmentConfig,
-        'production' : ProductionConfig
+        'production' : ProductionConfig,
+        'testing'    : TestingConfig
     }
